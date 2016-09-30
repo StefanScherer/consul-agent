54a38555164b079f54687150c95ff26249ce4dcc
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-30 16:41:46 +0200

    Check port 8500

--- packaging/msi/test/spec/ports_spec.rb
+++ packaging/msi/test/spec/ports_spec.rb
@@ -6,12 +6,20 @@ set :os, :family => 'windows'
 
 package = JSON.parse(File.read('../../../package.json'))
 
-if package['packaging'].has_key?('firewallExceptions')
-  describe "Ports" do
-    package['packaging']['firewallExceptions'].each do |firewall_exception|
-      describe port(firewall_exception['port'].to_i) do
-        it { should be_listening }
+describe "Ports" do
+  if package['packaging'].has_key?('firewallExceptions')
+    describe "Firewall ports" do
+      package['packaging']['firewallExceptions'].each do |firewall_exception|
+        describe port(firewall_exception['port'].to_i) do
+          it { should be_listening }
+        end
       end
+    end
+  end
+
+  describe "UI port" do
+    describe port(8500) do
+      it { should be_listening }
     end
   end
 end
